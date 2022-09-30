9794a422f5cd3bcb5556f6f25b9997567b371f49
weiym97 <weiym97@163.com>
2022-09-30 10:50:58 +0800

    new

--- plot_recovery.R
+++ plot_recovery.R
@@ -44,13 +44,11 @@ outlier <- args[3]
 stat_sim <- read.csv(paste('data/simulation/',data_name,'_statistics.csv',sep=''))
 stat_recov <- read.table(paste('fit_result/summary_',model_name,'_',data_name,'.txt',sep=''),header=T)
 
-dim(stat_sim)
-dim(stat_recov)
 
 
 if (outlier=='remove'){
-  #class(stat_recov$tau<200)
-  #stat_sim < - stat_sim[stat_recov$tau<200,]
+  temp <- stat_sim[stat_recov$tau<200,]
+  stat_sim < - temp
   stat_recov <- stat_recov[stat_recov$tau<200,]
 }
 
